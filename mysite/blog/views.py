aae2d5673c661db43263c9b1bb749eadbff2960d
sampleAppliTK <glazed.lib0721@gmail.com>
2020-09-11 09:37:25 +0900

    Modified templates to display posts from database.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.utils import timezone
+"""
+models の前にあるドットは カレントディレクトリ 、もしくは カレントアプリケーション のことです。
+views.pyと models.pyは、同じディレクトリに置いてあります。
+だから、こんな風に.とファイル名だけを使って、簡単に記述することが出来るのです。
+（ファイル名の拡張子.pyは必要ないです）
+そして、モデルの名前を指定してインポートします(この場合のモデルは Postですね)。
+"""
+from .models import Post # mysite\blog\models.py を Postという名前でインポート
 
 # Create your views here.
 
 # blog/urls.pyから呼び出される
 def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     # blog\templates\blog\post_list.html を読み込む
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html', {'posts': posts})

3120b1cc0ebf2ca4369c7b4c507cd4bc63b4f9b4
sampleAppliTK <glazed.lib0721@gmail.com>
2020-09-13 12:00:23 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,10 +2,16 @@ from django.urls import path
 from . import views # blog/views.pyをインポートする　ここではviews.py は、blogフォルダの中にしかないのでこれでOK
 
 
+
+# viewsクラスのメソッドは、views.pyで定義している
 urlpatterns = [
     # ローカルだとhttp://127.0.0.1:8000/
     # PythonAnywhereだとhttp://sampleapplitk.pythonanywhere.com/
     #　上記のリクエストが来たとき、views.post_listメソッドを実行する
     # post_list が行先であることをDjangoが伝える
     path('', views.post_list, name='post_list'),
+
+    # http://127.0.0.1:8000/post/1/　タイトルをクリックしたときのリンクを生成
+    # post_detail.html を読み込む。上記URLの場合　変数pkに 1 入れてから　post_detailメソッドを実行する
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+
 """
 models の前にあるドットは カレントディレクトリ 、もしくは カレントアプリケーション のことです。
 views.pyと models.pyは、同じディレクトリに置いてあります。
@@ -16,3 +18,7 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     # blog\templates\blog\post_list.html を読み込む
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
